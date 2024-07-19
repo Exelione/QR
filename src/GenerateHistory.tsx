b78292220adcc6b0bb9d51fb9b614527b20db3db
Exelione <exelione@gmail.com>
2024-07-19 22:26:36 +0300

    1.1.6

--- src/GenerateHistory.tsx
+++ src/GenerateHistory.tsx
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import QRCode from "qrcode.react";
 import Navigation from "./components/Navigation/Navigation";
 import { GENERATE_DATA } from "./constants";
 import styles from "./styles.module.css";
 
 const GenerateHistory = () => {
-    const history: string[] = JSON.parse(localStorage.getItem(GENERATE_DATA) || "[]");
-  return (
-    <div className={styles.container}>
-        <Navigation />
-        <h1>Generation History</h1>
-        {!history.length ? 
-        <p>No history found</p> : 
-        <ol>
-          {history.map((item)=> <li key={item}>
-          {<QRCode value={item} size={50} className={styles.qrCode} renderAs="svg"/>}
-            {item}
-          </li>
-          )}
-        </ol>
-        }
-    </div>
-  )
-}
+    const getHistory = () => JSON.parse(localStorage.getItem(GENERATE_DATA) || "[]");
+    const [history, setHistory] = useState(getHistory());
 
-export default GenerateHistory
+    const handleDelete = (itemToDelete: string) => {
+        const newHistory = history.filter((item : string) => item !== itemToDelete);
+        localStorage.setItem(GENERATE_DATA, JSON.stringify(newHistory));
+        setHistory(newHistory);
+    };
+
+    return (
+        <div className={styles.container}>
+            <Navigation />
+            <h1>Generation History</h1>
+            {!history.length ? 
+            <p>No history found</p> : 
+            <ol>
+                {history.map((item : string) => (
+                    <li key={item}>
+                        <QRCode value={item} size={50} className={styles.qrCode} renderAs="svg"/>
+                        {item}
+                        <button onClick={() => handleDelete(item)}>X</button>
+                    </li>
+                ))}
+            </ol>
+            }
+        </div>
+    );
+};
+
+export default GenerateHistory;
